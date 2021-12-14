3672694c33b33b296b4d9a5c15e20f8ce2ba506b
Mateusz Piórowicz <pioro199@gmail.com>
2021-12-14 21:33:55 +0100

    second commit

---
+++ acer.py
@@ -0,0 +1,14 @@
+from pc import PC
+class Acer(PC):
+    def __init__(self, name_company, cpu, ram, power_suplly):
+        super().__init__(name_company, cpu, ram, power_suplly)
+        
+    def __str__(self):
+        return super().__str__()
+         
+    
+acer=Acer("acer","intel core i7",16,-1)
+
+acer.components_name("cpu","ram")
+print(acer.checking_power_supply())
+print(acer)
---
+++ pc.py
@@ -0,0 +1,30 @@
+
+
+from abc import abstractmethod
+
+
+class PC:
+    def __init__(self,name_company,cpu,ram,power_supply):
+        self.name_company=name_company
+        self.cpu=cpu
+        self.ram=ram
+        self.power_supply=power_supply
+        
+           
+    def components_name(self,name_cpu,name_ram):
+        self.cpu=self.cpu, name_cpu
+        self.ram=self.ram, name_ram
+        
+            
+    @abstractmethod
+    def __str__(self):
+         return f"Name computer is: {self.name_company}, his cpu is: {self.cpu}, his ram is {self.ram} and power supply {self.power_supply}"
+     
+ 
+ 
+ 
+    def checking_power_supply(self):
+        if self.power_supply>0:
+            print("PC has positive supplies power")
+        else:
+            print("Ps has negative draws power")
